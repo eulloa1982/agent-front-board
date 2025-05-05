8e136121cfb5091a0c2e5ef1fbdf8beaa98e13c9
eulloa1982 <65827708+eulloa1982@users.noreply.github.com>
2025-05-05 18:46:54 -0400

    Agregando url personalizada al dashboard

--- src/utils/azureAuthUtil.js
+++ src/utils/azureAuthUtil.js
@@ -3,7 +3,7 @@ export const msalConfig = {
     auth: {
       clientId: "eba64229-dc11-4fdb-a6d9-d0d6888d20d1", // 👈 desde Azure Portal
       authority: "https://login.microsoftonline.com/7313ad10-b885-4b50-9c75-9dbbd975618f",
-      redirectUri: "https://ashy-coast-0a7ba170f.6.azurestaticapps.net/", // o tu dominio en Azure
+      redirectUri: "https://mydashboard.lmitservices.com/", // o tu dominio en Azure
     },
     cache: {
       cacheLocation: "sessionStorage", // o localStorage
