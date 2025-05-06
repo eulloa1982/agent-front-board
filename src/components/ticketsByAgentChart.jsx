fa6dba18b87136516b19e935bed8401edf370737
eulloa1982 <65827708+eulloa1982@users.noreply.github.com>
2025-05-05 20:55:22 -0400

    Update ticketsByAgentChart.jsx

--- src/components/ticketsByAgentChart.jsx
+++ src/components/ticketsByAgentChart.jsx
@@ -1,23 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend, CartesianGrid, ResponsiveContainer } from 'recharts';
 
-const data = [
-  { name: 'Carlos', tickets: 12 },
-  { name: 'Ana', tickets: 9 },
-  { name: 'Luis', tickets: 15 },
-  { name: 'Esteban', tickets: 7 },
-];
 
 const TicketsByAgentBarChart = () => {
+  const [tickets, setTickets] = useState([]);
+    const user = true;
+  
+    useEffect(() => {
+        if (user) {
+          //setLoadingTickets(true);
+    
+          fetch('https://prod-49.eastus.logic.azure.com:443/workflows/78c43496829e474abc793e946cb8464b/triggers/When_a_HTTP_request_is_received/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2FWhen_a_HTTP_request_is_received%2Frun&sv=1.0&sig=VECyw4EtDX7zMvY2aV9phII7VDII5qg7HocLSBgEMMc', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ userEmail: 'm' })
+          })
+            .then(res => {
+              if (!res.ok) throw new Error('Error al recuperar tickets');
+              return res.json();
+            })
+            .then(data => {
+              setTickets(data['ResultSets']['Table1'] || []);
+              //console.log(data['ResultSets']['Table1'])
+              //setLoadingTickets(false);
+            })
+            .catch(err => {
+              console.error('Error al cargar tickets:', err);
+              //setErrorTickets(err);
+              //setLoadingTickets(false);
+            });
+        }
+      }, [user]);
+  
+  
+  
+
+
+
   return (
     <ResponsiveContainer width="100%" height={300}>
-      <BarChart data={data} margin={{ top: 20, right: 30, left: 0, bottom: 5 }}>
+      <BarChart data={tickets} margin={{ top: 20, right: 30, left: 0, bottom: 5 }}>
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey="name" />
         <YAxis allowDecimals={false} />
         <Tooltip />
         <Legend />
-        <Bar dataKey="tickets" fill="#3f51b5" />
+        <Bar dataKey="tickets_this_week" fill="#3f51b5" />
       </BarChart>
     </ResponsiveContainer>
   );
