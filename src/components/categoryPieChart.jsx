3f2f5733f8563acb13151fcf439f069332860eae
eulloa1982 <65827708+eulloa1982@users.noreply.github.com>
2025-05-05 21:15:57 -0400

    Update categoryPieChart.jsx

--- src/components/categoryPieChart.jsx
+++ src/components/categoryPieChart.jsx
@@ -1,12 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { PieChart, Pie, Cell, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
-const data = [
-  { name: 'Soporte', value: 12 },
-  { name: 'Finanzas', value: 8 },
-  { name: 'TI', value: 15 },
-];
-
 const COLORS = ['#8884d8', '#82ca9d', '#ffc658'];
 
 const CategoryPieChart = () => {
