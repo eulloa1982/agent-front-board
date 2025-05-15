aa93d7c40790329e9e3ea7bcda39e81418e0526e
lmmcdev <subscription@clmmail.com>
2025-05-15 14:34:37 -0400

    Update msalProviderWrapper.js

--- src/utils/msalProviderWrapper.js
+++ src/utils/msalProviderWrapper.js
@@ -1,23 +1,16 @@
 
 // src/MsalProviderWrapper.jsx
 import React from "react";
-import { PublicClientApplication, InteractionType } from "@azure/msal-browser";
-import { MsalProvider, MsalAuthenticationTemplate } from "@azure/msal-react";
-import { msalConfig, loginRequest } from "./azureAuthUtil";
+import { PublicClientApplication } from "@azure/msal-browser";
+import { MsalProvider } from "@azure/msal-react";
+import { msalConfig } from "./azureAuthUtil";
 
 const msalInstance = new PublicClientApplication(msalConfig);
 
 export default function MsalProviderWrapper({ children }) {
   return (
     <MsalProvider instance={msalInstance}>
-      <MsalAuthenticationTemplate
-        interactionType={InteractionType.Popup}
-        authenticationRequest={loginRequest}
-        loadingComponent={<div>Cargando autenticación...</div>}
-        errorComponent={<div>Error al autenticar</div>}
-      >
         {children}
-      </MsalAuthenticationTemplate>
     </MsalProvider>
   );
 }
